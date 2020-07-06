e5bfb9c7441224f3a08a28a3171c51df93a8b58f
Gustavo <gustavo.marin@intelygenz.com>
2020-07-06 12:07:44 +0200

    feat(kre-py): runners support cuda
    * fix: refactor runners to new folder structure specified on #151
    
    * add cuda support for kre-py runner. Related to #159
    
    * fix krectl to clean docker images. fix hostctl check.

---
+++ kre-go/config/config.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"os"
+
+	"github.com/konstellation-io/kre/libs/simplelogger"
+)
+
+type Config struct {
+	VersionID string
+	Version   string
+	NodeName  string
+	BasePath  string
+	NATS      ConfigNATS
+	MongoDB   MongoDB
+}
+
+type MongoDB struct {
+	Address     string
+	DBName      string
+	ConnTimeout int
+}
+
+type ConfigNATS struct {
+	Server             string
+	InputSubject       string
+	OutputSubject      string
+	MongoWriterSubject string
+}
+
+func NewConfig(logger *simplelogger.SimpleLogger) Config {
+	return Config{
+		VersionID: getCfgFromEnv(logger, "KRT_VERSION_ID"),
+		Version:   getCfgFromEnv(logger, "KRT_VERSION"),
+		NodeName:  getCfgFromEnv(logger, "KRT_NODE_NAME"),
+		BasePath:  getCfgFromEnv(logger, "KRT_BASE_PATH"),
+		NATS: ConfigNATS{
+			Server:             getCfgFromEnv(logger, "KRT_NATS_SERVER"),
+			InputSubject:       getCfgFromEnv(logger, "KRT_NATS_INPUT"),
+			OutputSubject:      getCfgFromEnv(logger, "KRT_NATS_OUTPUT"),
+			MongoWriterSubject: getCfgFromEnv(logger, "KRT_NATS_MONGO_WRITER"),
+		},
+		MongoDB: MongoDB{
+			Address:     getCfgFromEnv(logger, "KRT_MONGO_URI"),
+			DBName:      getCfgFromEnv(logger, "KRT_MONGO_DB_NAME"),
+			ConnTimeout: 120,
+		},
+	}
+}
+
+func getCfgFromEnv(logger *simplelogger.SimpleLogger, name string) string {
+	val, ok := os.LookupEnv(name)
+	if !ok {
+		logger.Errorf("Error reading config: the '%s' env var is missing", name)
+		os.Exit(1)
+	}
+	return val
+}
---
+++ kre-py/src/main.py
@@ -0,0 +1,133 @@
+import asyncio
+import datetime
+import importlib.util
+import os
+import sys
+import time
+import traceback
+import pymongo
+import inspect
+
+from kre_nats import KreNatsMessage
+from kre_context import HandlerContext
+from kre_runner import Runner
+
+NATS_FLUSH_TIMEOUT = 10
+
+
+class Config:
+    def __init__(self):
+        # Mandatory variables
+        try:
+            self.krt_version_id = os.environ['KRT_VERSION_ID']
+            self.krt_version = os.environ['KRT_VERSION']
+            self.krt_node_name = os.environ['KRT_NODE_NAME']
+            self.nats_server = os.environ['KRT_NATS_SERVER']
+            self.nats_input = os.environ['KRT_NATS_INPUT']
+            self.nats_output = os.environ['KRT_NATS_OUTPUT']
+            self.nats_mongo_writer = os.environ['KRT_NATS_MONGO_WRITER']
+            self.base_path = os.environ['KRT_BASE_PATH']
+            self.handler_path = os.environ['KRT_HANDLER_PATH']
+            self.mongo_db_name = os.environ['KRT_MONGO_DB_NAME']
+            self.mongo_uri = os.environ['KRT_MONGO_URI']
+            self.nats_flush_timeout = NATS_FLUSH_TIMEOUT
+
+        except Exception as err:
+            raise Exception(f"error reading config: the {str(err)} env var is missing")
+
+
+class NodeRunner(Runner):
+    def __init__(self):
+        config = Config()
+        name = f"{config.krt_version}-{config.krt_node_name}"
+        Runner.__init__(self, name, config)
+        self.handler_ctx = None
+        self.handler_fn = None
+
+    async def process_messages(self):
+        self.logger.info(f"connecting to MongoDB...")
+        self.mongo_conn = pymongo.MongoClient(self.config.mongo_uri, socketTimeoutMS=10000, connectTimeoutMS=10000)
+
+        queue_name = f"queue_{self.config.nats_input}"
+        self.logger.info(f"listening to '{self.config.nats_input}' subject with queue '{queue_name}'")
+
+        self.subscription_sid = await self.nc.subscribe(
+            self.config.nats_input,
+            cb=self.create_message_cb(),
+            queue=queue_name
+        )
+        await self.prepare_handler()
+
+    async def prepare_handler(self):
+        handler_full_path = os.path.join(self.config.base_path, self.config.handler_path)
+        handler_dirname = os.path.dirname(handler_full_path)
+        sys.path.append(handler_dirname)
+        spec = importlib.util.spec_from_file_location("worker", handler_full_path)
+        handler_module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(handler_module)
+
+        if not hasattr(handler_module, "handler"):
+            raise Exception(f"handler module '{handler_full_path}' must implement a function 'handler(ctx, data)'")
+        else:
+            self.logger.info(f"handler script was loaded from '{handler_full_path}'")
+
+        self.handler_fn = handler_module.handler
+        self.handler_ctx = HandlerContext(self.config, self.nc, self.mongo_conn, self.logger)
+
+        if not hasattr(handler_module, "init"):
+            return
+
+        if inspect.iscoroutinefunction(handler_module.init):
+            await asyncio.create_task(handler_module.init(self.handler_ctx))
+        else:
+            handler_module.init(self.handler_ctx)
+
+    def create_message_cb(self):
+        async def message_cb(msg):
+            start = time.time()
+            try:
+                request_msg = KreNatsMessage(msg=msg)
+
+                if msg.reply == "" and request_msg.reply == "":
+                    raise Exception("the reply subject was not found")
+
+                if msg.data is None:
+                    raise Exception("message data can't be null")
+
+                if msg.reply != "":
+                    request_msg.reply = msg.reply
+
+                self.logger.info(f"received message on '{msg.subject}' with final reply '{request_msg.reply}'")
+
+                handler_result = await self.handler_fn(self.handler_ctx, request_msg.data)
+
+                is_last_node = self.config.nats_output == ''
+                output_subject = request_msg.reply if is_last_node else self.config.nats_output
+                response_msg = KreNatsMessage(reply=request_msg.reply, data=handler_result)
+
+                await self.nc.publish(output_subject, response_msg.marshal())
+                self.logger.info(f"published response to '{output_subject}' with final reply '{request_msg.reply}'")
+
+                await self.nc.flush(timeout=self.config.nats_flush_timeout)
+
+                end = time.time()
+                self.logger.info(f"version[{self.handler_ctx.__config__.krt_version}] "
+                                 f"node[{self.handler_ctx.__config__.krt_node_name}] "
+                                 f"reply[{request_msg.reply}] "
+                                 f"start[{datetime.datetime.utcfromtimestamp(start).isoformat()}] "
+                                 f"end[{datetime.datetime.utcfromtimestamp(end).isoformat()}] "
+                                 f"elapsed[{round(end - start, 2)}]"
+                                 )
+
+            except Exception as err:
+                traceback.print_exc()
+                self.logger.error("error executing handler:" + str(err))
+                response_msg = KreNatsMessage(error=f"error in '{self.config.krt_node_name}': {str(err)}")
+                await self.nc.publish(request_msg.reply, response_msg.marshal())
+
+        return message_cb
+
+
+if __name__ == '__main__':
+    runner = NodeRunner()
+    runner.start()
