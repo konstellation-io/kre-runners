9fb7d7634a64e443032b35777e23c9314a92e555
Josemi Hernandez <josemihdezgarcia@gmail.com>
2022-05-25 16:27:20 +0100

    Minor change

--- kre-py/src/config.py
+++ kre-py/src/config.py
@@ -20,6 +20,6 @@ class Config:
             self.mongo_data_db_name = "data"
             self.mongo_uri = os.environ['KRT_MONGO_URI']
             self.influx_uri = os.environ['KRT_INFLUX_URI']
-            self.krt_last_node = os.environ['KRT_LAST_NODE']
+            self.krt_last_node = os.environ['KRT_IS_LAST_NODE']
         except Exception as err:
             raise Exception(f"error reading config: the {str(err)} env var is missing")
