de7587770967f2a310c89110f0eec6b6bed29ae8
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2022-10-24 17:56:44 +0200

    feat(kre-go): refactor and upgrade ctx data test

--- kre-go/context_data_test.go
+++ kre-go/context_data_test.go
@@ -4,118 +4,46 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"os"
 	"testing"
 	"time"
 
-	"github.com/nats-io/nats.go"
-
 	"github.com/golang/mock/gomock"
-	"github.com/konstellation-io/kre/libs/simplelogger"
+	"github.com/nats-io/nats-server/v2/server"
 	testserver "github.com/nats-io/nats-server/v2/test"
+	"github.com/nats-io/nats.go"
+	"github.com/stretchr/testify/suite"
+
 	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/konstellation-io/kre-runners/kre-go/config"
 	"github.com/konstellation-io/kre-runners/kre-go/mocks"
+	"github.com/konstellation-io/kre/libs/simplelogger"
 )
 
-type TestPrediction struct {
-	Time       time.Time
-	Prediction string
-	TicketID   string
-	Asset      string
+type TestData struct {
+	Time     time.Time
+	Result   string
+	TicketID string
+	Asset    string
 }
 
-func setEnvVars(t *testing.T, envVars map[string]string) {
-	for name, value := range envVars {
-		err := os.Setenv(name, value)
-		if err != nil {
-			t.Fatal(err)
-		}
-	}
+type ContextDataTestSuite struct {
+	suite.Suite
+	tServer *server.Server
+	nc      *nats.Conn
+	mongoM  *mocks.MockManager
+	ctrl    *gomock.Controller
+	ctxData *contextData
 }
 
-func TestHandlerContext_GetData(t *testing.T) {
-	logger := simplelogger.New(simplelogger.LevelDebug)
-	cfg := config.Config{}
+const mongoWriterSubject = "mongo_writer"
 
-	testPort := 8331
-	opts := testserver.DefaultTestOptions
-	opts.Port = testPort
-	s := testserver.RunServer(&opts)
-	defer s.Shutdown()
-
-	nc, err := nats.Connect(fmt.Sprintf("nats://127.0.0.1:%d", testPort))
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer nc.Close()
-
-	msgCh := make(chan *nats.Msg, 64)
-
-	sub, err := nc.ChanSubscribe("mongo_writer", msgCh)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer sub.Unsubscribe()
-
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	mongoM := mocks.NewMockManager(ctrl)
-
-	ctx := NewHandlerContext(cfg, nc, mongoM, logger, nil, nil)
-
-	q := QueryData{
-		"TicketID": "1234",
-		"Asset":    "A5678",
-	}
-
-	var results []*TestPrediction
-
-	savedData := TestPrediction{
-		Time:       time.Now(),
-		Prediction: "Repair Complete",
-		TicketID:   "1234",
-		Asset:      "A5678",
-	}
-
-	criteria := bson.M{
-		"TicketID": "1234",
-		"Asset":    "A5678",
-	}
-
-	mongoM.EXPECT().
-		Find(gomock.Any(), "test_predictions", criteria, results).
-		Return(nil).
-		Do(func(ctx context.Context, colName string, filter interface{}, _ interface{}) {
-			results = append(results, &TestPrediction{
-				Time:       time.Now(),
-				Prediction: "Repair Complete",
-				TicketID:   "1234",
-				Asset:      "A5678",
-			})
-		})
-
-	err = ctx.DB.Find("test_predictions", q, results)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if len(results) == 0 {
-		t.Fatalf("no result returned")
-	}
-	result := results[0]
-	if result.Prediction != savedData.Prediction {
-		t.Fatalf("saved data is wrong: %s != %s", result.Prediction, savedData.Prediction)
-	}
+func TestContextDataTestSuite(t *testing.T) {
+	suite.Run(t, new(ContextDataTestSuite))
 }
 
-func TestHandlerContext_SaveData(t *testing.T) {
-	const mongoWriterSubject = "mongo_writer"
-
-	logger := simplelogger.New(simplelogger.LevelDebug)
-
+func (suite *ContextDataTestSuite) SetupSuite() {
+	logger := simplelogger.New(simplelogger.LevelInfo)
 	cfg := config.Config{
 		NATS: config.ConfigNATS{
 			MongoWriterSubject: mongoWriterSubject,
@@ -128,39 +56,82 @@ func TestHandlerContext_SaveData(t *testing.T) {
 	testPort := 8331
 	opts := testserver.DefaultTestOptions
 	opts.Port = testPort
-	s := testserver.RunServer(&opts)
-	defer s.Shutdown()
+	suite.tServer = testserver.RunServer(&opts)
 
-	nc, err := nats.Connect(fmt.Sprintf("nats://127.0.0.1:%d", testPort))
-	if err != nil {
-		t.Fatal(err)
+	var err error
+	suite.nc, err = nats.Connect(fmt.Sprintf("nats://127.0.0.1:%d", testPort))
+	suite.Require().NoError(err)
+
+	suite.ctrl = gomock.NewController(suite.T())
+	suite.mongoM = mocks.NewMockManager(suite.ctrl)
+
+	suite.ctxData = NewContextData(cfg, suite.nc, suite.mongoM, logger)
+}
+
+func (suite *ContextDataTestSuite) TearDownSuite() {
+	suite.ctrl.Finish()
+	suite.nc.Close()
+	suite.tServer.Shutdown()
+}
+
+func (suite *ContextDataTestSuite) TestContextDataFind() {
+	qData := QueryData{
+		"TicketID": "1234",
+		"Asset":    "A5678",
 	}
-	defer nc.Close()
 
+	var results []*TestData
+
+	savedData := TestData{
+		Time:     time.Now(),
+		Result:   "Repair Complete",
+		TicketID: "1234",
+		Asset:    "A5678",
+	}
+
+	criteria := bson.M{
+		"TicketID": "1234",
+		"Asset":    "A5678",
+	}
+
+	suite.mongoM.EXPECT().
+		Find(gomock.Any(), "test_data", criteria, results).
+		Return(nil).
+		Do(func(ctx context.Context, colName string, filter interface{}, _ interface{}) {
+			results = append(results, &TestData{
+				Time:     time.Now(),
+				Result:   "Repair Complete",
+				TicketID: "1234",
+				Asset:    "A5678",
+			})
+		})
+
+	err := suite.ctxData.Find("test_data", qData, results)
+	suite.Require().NoError(err)
+
+	suite.Require().Len(results, 1)
+	result := results[0]
+	suite.Equal(savedData.Result, result.Result)
+}
+
+func (suite *ContextDataTestSuite) TestContextDataSave() {
 	msgCh := make(chan *nats.Msg, 64)
-
-	sub, err := nc.ChanSubscribe(mongoWriterSubject, msgCh)
-	if err != nil {
-		t.Fatal(err)
-	}
+	sub, err := suite.nc.ChanSubscribe(mongoWriterSubject, msgCh)
+	suite.Require().NoError(err)
 	defer sub.Unsubscribe()
 
-	ctrl := gomock.NewController(t)
-	mongoM := mocks.NewMockManager(ctrl)
-
-	ctx := NewHandlerContext(cfg, nc, mongoM, logger, nil, nil)
-	sentMsg := TestPrediction{
-		Time:       time.Now(),
-		Prediction: "Tested",
-		TicketID:   "1234",
-		Asset:      "A12345C",
+	sentMsg := TestData{
+		Time:     time.Now(),
+		Result:   "Tested",
+		TicketID: "1234",
+		Asset:    "A12345C",
 	}
 
 	c, cancel := context.WithCancel(context.Background())
 
 	var goFuncErr error
 	go func() {
-		err := ctx.DB.Save("test_predictions", sentMsg)
+		err := suite.ctxData.Save("test_data", sentMsg)
 		goFuncErr = err
 		cancel()
 	}()
@@ -168,28 +139,18 @@ func TestHandlerContext_SaveData(t *testing.T) {
 	receivedMsg := SaveDataMsg{}
 
 	msg := <-msgCh
-	if goFuncErr != nil {
-		t.Fatal(goFuncErr)
-	}
+	suite.Require().NoError(goFuncErr)
 
 	err = json.Unmarshal(msg.Data, &receivedMsg)
-	if err != nil {
-		t.Fatalf("Error parsing data msg: %s", err)
-	}
+	suite.Require().NoError(err)
 
 	err = msg.Respond([]byte("{ Success: true }"))
-	if err != nil {
-		t.Fatalf("Error replaying to the data msg: %s", err)
-	}
+	suite.Require().NoError(err)
 
 	<-c.Done()
 
 	receivedDoc := receivedMsg.Doc.(map[string]interface{})
 
-	if receivedDoc["TicketID"] != sentMsg.TicketID {
-		t.Fatalf("TicketID value is wrong %s != %s", receivedDoc["TicketID"], sentMsg.TicketID)
-	}
-	if receivedDoc["Asset"] != sentMsg.Asset {
-		t.Fatalf("Asset value is wrong %s != %s", receivedDoc["Asset"], sentMsg.Asset)
-	}
+	suite.Assert().Equal(sentMsg.TicketID, receivedDoc["TicketID"])
+	suite.Assert().Equal(sentMsg.Asset, receivedDoc["Asset"])
 }
