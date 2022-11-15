ac2e4c2a2f6aa913eecf584b404250884153c029
Alex Rodriguez Fernandez <alex.rodriguez@intelygenz.com>
2022-11-15 18:04:21 +0100

    feat(kre-py): krt-v2 python runner support (#124)
    BREAKING CHANGE: change python runner to krt v2 support

--- kre-py/src/config.py
+++ kre-py/src/config.py
@@ -12,7 +12,7 @@ class Config:
             self.krt_version = os.environ["KRT_VERSION"]
             self.krt_node_name = os.environ["KRT_NODE_NAME"]
             self.nats_server = os.environ["KRT_NATS_SERVER"]
-            self.nats_input = os.environ["KRT_NATS_INPUT"]
+            self.nats_inputs = os.environ["KRT_NATS_INPUTS"].split(",")
             self.nats_output = os.environ["KRT_NATS_OUTPUT"]
             self.nats_stream = os.environ["KRT_NATS_STREAM"]
             self.nats_mongo_writer = os.environ["KRT_NATS_MONGO_WRITER"]
@@ -21,7 +21,6 @@ class Config:
             self.mongo_data_db_name = "data"
             self.mongo_uri = os.environ["KRT_MONGO_URI"]
             self.influx_uri = os.environ["KRT_INFLUX_URI"]
-            self.krt_last_node = os.environ["KRT_IS_LAST_NODE"]
-            self.nats_entrypoint_subject = os.environ["KRT_NATS_ENTRYPOINT_SUBJECT"]
+            self.is_exitpoint = bool(os.environ["KRT_IS_EXITPOINT"])
         except Exception as err:
             raise Exception(f"error reading config: the {str(err)} env var is missing")
--- kre-py/src/context.py
+++ kre-py/src/context.py
@@ -1,15 +1,25 @@
 import os
+from typing import Callable, Any, Awaitable
+
+from google.protobuf.message import Message
 
 from context_measurement import ContextMeasurement
 from context_prediction import ContextPrediction
 from context_data import ContextData
+from kre_nats_msg_pb2 import ERROR, OK, MessageType, EARLY_REPLY, EARLY_EXIT
+
+PublishMsgFunc = Callable[[Message, Any, MessageType, str], Awaitable]
+PublishAnyFunc = Callable[[Message, Any, MessageType, str], Awaitable]
+
+DEFAULT_CHANNEL = ""
 
 
 class HandlerContext:
-    def __init__(self, config, nc, mongo_conn, logger, early_reply_func):
+    def __init__(self, config, nc, mongo_conn, logger, publish_msg: PublishMsgFunc, publish_any: PublishAnyFunc):
         self.__data__ = lambda: None
         self.__config__ = config
-        self.__early_reply__ = early_reply_func
+        self.__publish_msg__ = publish_msg
+        self.__publish_any__ = publish_any
         self.__request_msg__ = None
         self.logger = logger
         self.prediction = ContextPrediction(config, nc, logger)
@@ -25,12 +35,76 @@ class HandlerContext:
     def get(self, key: str) -> any:
         return getattr(self.__data__, key)
 
-    async def early_reply(self, response):
-        if self.__request_msg__.replied:
-            raise Exception("error the message was replied previously")
+    def set_request_msg(self, request_msg):
+        self.__request_msg__ = request_msg
 
-        self.__request_msg__.replied = True
-        await self.__early_reply__(response, self.__request_msg__.reply)
+    def get_request_id(self) -> str:
+        """get_request_id will return the payload's original request ID."""
+        return self.__request_msg__.request_id
 
-    def early_exit(self):
-        self.__request_msg__.early_exit = True
+    async def send_output(self, message: Message, channel: str = DEFAULT_CHANNEL):
+        """
+        send_output will send a desired typed proto payload to the node's subject.
+        By specifying a channel, the message will be sent to that subject's subtopic.
+
+        send_output converts the proto message into an any type. This means the following node will recieve
+        an any type protobuf.
+
+        GRPC requests can only be answered once. So once the entrypoint has been replied by the exitpoint,
+        all following replies to the entrypoint from the same request will be ignored.
+
+        :param  message:  The message to publish.
+        :param channel (optional):  The subsubject channel where the message will be published.
+        """
+        await self.__publish_msg__(message, self.__request_msg__, OK, channel)
+
+    async def send_any(self, message, channel: str = DEFAULT_CHANNEL):
+        """
+        send_any will send any type of proto payload to the node's subject.
+        By specifying a channel, the message will be sent to that subject's subtopic.
+
+        As a difference from SendOutput, send_any will not convert your proto structure.
+
+        Use this function when you wish to simply redirect your node's payload without unpackaging.
+        Once the entrypoint has been replied, all following replies to the entrypoint will be ignored.
+
+        :param  message:  The message to publish.
+        :param channel (optional):  The subsubject channel where the message will be published.
+        """
+        await self.__publish_any__(message, self.__request_msg__, OK, channel)
+
+    async def send_early_reply(self, message, channel: str = DEFAULT_CHANNEL):
+        """
+        send_early_reply works as the SendOutput functionality
+        with the addition of typing this message as an early reply.
+
+        :param  message:  The message to publish.
+        :param channel (optional):  The subsubject channel where the message will be published.
+        """
+        await self.__publish_msg__(message, self.__request_msg__, EARLY_REPLY, channel)
+
+    async def send_early_exit(self, message, channel: str = DEFAULT_CHANNEL):
+        """
+        send_early_exit works as the SendOutput functionality
+        with the addition of typing this message as an early exit.
+
+        :param  message:  The message to publish.
+        :param channel (optional):  The subsubject channel where the message will be published.
+        """
+        await self.__publish_msg__(message, self.__request_msg__, EARLY_EXIT, channel)
+
+    def is_message_ok(self) -> bool:
+        """is_message_ok returns true if incoming message is of message type OK."""
+        return self.__request_msg__.message_type == OK
+
+    def is_message_error(self) -> bool:
+        """is_message_error returns true if incoming message is of message type ERROR."""
+        return self.__request_msg__.message_type == ERROR
+
+    def is_message_early_reply(self) -> bool:
+        """is_message_early_reply returns true if incoming message is of message type EARLY_REPLY."""
+        return self.__request_msg__.message_type == EARLY_REPLY
+
+    def is_message_early_exit(self) -> bool:
+        """is_message_early_exit returns true if incoming message is of message type EARLY_EXIT."""
+        return self.__request_msg__.message_type == EARLY_EXIT
