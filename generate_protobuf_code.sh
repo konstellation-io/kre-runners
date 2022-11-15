14c5432087c330fb73711c81b044fc125a46a105
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2022-11-15 18:16:10 +0100

    feat(chore): deprecate protobuf generate

--- generate_protobuf_code.sh
+++ generate_protobuf_code.sh
@@ -4,7 +4,6 @@ protoc -I=proto \
   --go_out=kre-go \
   --go_opt=paths=source_relative \
   --python_out=kre-py/src \
-  --python_out=kre-entrypoint/src \
-  --python_out=e2e/test_assets  proto/kre_nats_msg.proto
+  --python_out=kre-entrypoint/src proto/kre_nats_msg.proto
 
 echo "done"
