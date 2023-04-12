0c6885b1747c003245fd23e0eb1d236eb6822696
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2023-04-12 10:43:08 +0200

    fix merge

--- e2e/test_assets/nodeC-vol/src/node/main.go
+++ e2e/test_assets/nodeC-vol/src/node/main.go
@@ -55,67 +55,6 @@ func handler(ctx *kre.HandlerContext, data *anypb.Any) error {
 	res.TestingResults = testingResults
 	ctx.SendOutput(res)
 
-	if randomInt := rand.Intn(100); randomInt < 5 {
-		ctx.Logger.Info("testing object store")
-		err = testObjectStore(ctx)
-		if err != nil {
-			return fmt.Errorf("error during test object store: %w", err)
-		}
-
-		ctx.Logger.Info("testing configuration")
-		err = testKVStore(ctx)
-		if err != nil {
-			return fmt.Errorf("error during test key value store: %w", err)
-		}
-	}
-	return nil
-}
-
-func testObjectStore(ctx *kre.HandlerContext) error {
-	objKey := "test_object_key"
-	objValue := fmt.Sprintf("%d", rand.Intn(100000))
-
-	err := ctx.ObjectStore.Save(objKey, []byte(objValue))
-	if err != nil {
-		return err
-	}
-
-	objRes, err := ctx.ObjectStore.Get(objKey)
-	if err != nil {
-		return err
-	}
-
-	ctx.Logger.Infof("the value from the object store is: %s", string(objRes))
-
-	ctx.ObjectStore.Delete(objKey)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func testKVStore(ctx *kre.HandlerContext) error {
-	kvKey := "test_kv_key"
-	objValue := fmt.Sprintf("Test Node Value: %d", rand.Intn(100000))
-
-	err := ctx.Configuration.Set(kvKey, objValue)
-	if err != nil {
-		return err
-	}
-
-	kvRes, err := ctx.Configuration.Get(kvKey, kre.ScopeNode)
-	if err != nil {
-		return err
-	}
-
-	ctx.Logger.Infof("the config from the kvStore is: %s", kvRes)
-
-	ctx.Configuration.Delete(kvKey)
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
 
